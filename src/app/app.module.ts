e88a23db799a8449feb0c66808c69f16f9609dfe
abdelrahmman97 <mabdelrahmman22@gmail.com>
2024-04-29 14:16:07 +0300

    init project arch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+export function HttpLoaderFactory ( http: HttpClient ) {
+	return new TranslateHttpLoader( http, "./assets/i18n/", ".json" );
+}
+
+@NgModule( {
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		LoadingBarHttpClientModule,
+		LoadingBarRouterModule,
+		TranslateModule.forRoot(
+			{
+				defaultLanguage: 'en',
+				loader: {
+					provide: TranslateLoader,
+					useFactory: HttpLoaderFactory,
+					deps: [ HttpClient ]
+				}
+			}
+		),
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
