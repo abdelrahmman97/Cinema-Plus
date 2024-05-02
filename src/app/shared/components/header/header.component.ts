7917c91fe915219b719354a827b1330788fe996c
abdelrahmman97 <mabdelrahmman22@gmail.com>
2024-05-02 05:09:21 +0300

    updated header design

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
-})
+@Component( {
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.css'
+} )
 export class HeaderComponent {
-
+	links: any[] = [
+		{ name: 'Movies', path: 'movies' },
+		{ name: 'TV Shows', path: 'tv-shows' },
+		{ name: 'People', path: 'people' }
+	];
 }
