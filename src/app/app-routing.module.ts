bb11c9655b860a4a149fe2d4d5e3067ad54bdf65
abdelrahmman97 <mabdelrahmman22@gmail.com>
2024-05-03 02:10:35 +0300

    created rounting & landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		loadChildren: () => import( './features/features.module' ).then( m => m.FeaturesModule )
+	},
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+@NgModule( {
+	imports: [ RouterModule.forRoot( routes ) ],
+	exports: [ RouterModule ]
+} )
 export class AppRoutingModule { }
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LandingComponent } from './components/landing/landing.component';
+import { FeaturesRoutingModule } from './features-routing.module';
 
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
-})
+@NgModule( {
+	declarations: [
+		LandingComponent
+	],
+	imports: [
+		CommonModule,
+		FeaturesRoutingModule
+	]
+} )
 export class FeaturesModule { }
